9089c6c1ef061521c3182ffb978692f112940f7a
Debopam Roy <rhevehjsns@gmail.com>
2026-03-12 15:55:31 +0800

    Fix checksyle violations in GitHub.java

--- src/main/java/seedu/address/model/person/GitHub.java
+++ src/main/java/seedu/address/model/person/GitHub.java
@@ -4,23 +4,33 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- *
+ * Represents a Person's GitHub username in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidGitHub(String)}
  */
 public class GitHub {
+
     public static final String MESSAGE_CONSTRAINTS =
-            "GitHub username must only contain alphaneumeric characters or hyphens, "
+            "GitHub username must only contain alphanumeric characters or hyphens, "
             + "and cannot start or end with a hyphen.";
 
     public static final String VALIDATION_REGEX = "[a-zA-Z0-9]([a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}";
 
     public final String value;
 
+    /**
+     * Constructs a {@code GitHub}.
+     *
+     * @param github A valid GitHub username.
+     */
     public GitHub(String github) {
         requireNonNull(github);
         checkArgument(isValidGitHub(github), MESSAGE_CONSTRAINTS);
         this.value = github;
     }
 
+    /**
+     * Returns true if a given string is a valid GitHub username.
+     */
     public static boolean isValidGitHub(String test) {
         return test.matches(VALIDATION_REGEX);
     }
@@ -32,11 +42,22 @@ public class GitHub {
 
     @Override
     public boolean equals(Object other) {
-        return other == this || (other instanceof GitHub && value.equals(((GitHub) other).value));
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GitHub)) {
+            return false;
+        }
+
+        GitHub otherGitHub = (GitHub) other;
+        return value.equals(otherGitHub.value);
     }
 
     @Override
     public int hashCode() {
         return value.hashCode();
     }
+
 }
